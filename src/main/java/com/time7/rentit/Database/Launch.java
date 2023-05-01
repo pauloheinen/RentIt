8a8f4c3c440f0a82fdcc76cbc709c718a411b981
Paulo Henrique <paulohenriqe226@gmail.com>
2023-05-01 09:00:26 -0300

    repo start

---
+++ src/main/java/com/time7/rentit/Database/Launch.java
@@ -0,0 +1,13 @@
+package com.time7.rentit.Database;
+
+/**
+ *
+ * @author Paulo Henrique
+ */
+public class Launch {
+    
+    public static void main( String[] args )
+    {
+        System.out.println("hello world!");
+    }
+}
