4c00a6c036cb1164574f99d9f98bfce441727fde
Daniel Kistemacher da Silva <106277555+DanielKistemacher@users.noreply.github.com>
2023-05-01 18:41:20 -0300

    0.3 Criação das classes de Entity, Service e Implementation

---
+++ src/main/java/com/time7/rentit/Entity/Employee.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.time7.rentit.Entity;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Employee {
+    private Long id;
+    private String name;
+    private String userName;
+    private String password;
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
