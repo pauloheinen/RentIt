7a87d2b46fb2730d6df88e7b906a95f1bd6184b6
Daniel Kistemacher da Silva <106277555+DanielKistemacher@users.noreply.github.com>
2023-05-01 10:06:47 -0300

    0.0 Alteração Classe Veiculo

--- src/main/java/com/time7/rentit/Entity/Veiculo.java
+++ src/main/java/com/time7/rentit/Entity/Veiculo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.time7.rentit.Entity;
 
 /**
@@ -9,5 +5,5 @@ package com.time7.rentit.Entity;
  * @author Daniel
  */
 public class Veiculo {
-    
+
 }
