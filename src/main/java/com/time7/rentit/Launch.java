e655f245c7c3b696c12e3e90d1dbd9e701a18d80
Jose Augusto Scherer <jose.augusto.github@gmail.com>
2023-05-02 22:34:44 -0300

    0.4 Ajustado o ícone do LoginPane

--- src/main/java/com/time7/rentit/Launch.java
+++ src/main/java/com/time7/rentit/Launch.java
@@ -2,9 +2,6 @@ package com.time7.rentit;
 
 import com.time7.rentit.Panes.LoginPane;
 import com.time7.rentit.Panes.RentItWindow;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import javax.swing.JOptionPane;
 
 /**
  *
@@ -12,43 +9,10 @@ import javax.swing.JOptionPane;
  */
 public class Launch {
     
-    static Connection conexao = null;
-    
     public static void main( String[] args ) {
-                
-        if (getConnection()){
-            // java.awt.EventQueue.invokeLater( () -> {
-                //new RentItWindow().setVisible(true);
-            //});
-            new LoginPane().setVisible(true);
-        } else {
-            JOptionPane.showMessageDialog(null, "Erro ao conectar ao banco de dados", "Erro", 0);
-        }
+        java.awt.EventQueue.invokeLater( () -> {
+            new RentItWindow().setVisible(true);
+        });
     }
-    
-    private static boolean getConnection() {
-        try {
-            String dbdriver = "com.mysql.jdbc.Driver";
-            String dburl = "jdbc:mysql://localhost/rentitdb";
-            String dbuser = "root";
-            String dbsenha = "280519";
-
-            // Carrega Driver do Banco de Dados
-            Class.forName(dbdriver);
-
-            if (dbuser.length() != 0) // conexão COM usuário e senha
-            {
-                conexao = DriverManager.getConnection(dburl, dbuser, dbsenha);
-            } else // conexão SEM usuário e senha
-            {
-                conexao = DriverManager.getConnection(dburl);
-            }
-
-            return true;
-
-        } catch (Exception e) {
-            System.err.println("Erro ao tentar conectar: " + e);
-            return false;
-        }
-    }
+   
 }
--- src/main/java/com/time7/rentit/Panes/RentItWindow.java
+++ src/main/java/com/time7/rentit/Panes/RentItWindow.java
@@ -1,38 +1,79 @@
 package com.time7.rentit.Panes;
 
+import java.awt.BorderLayout;
+
 /**
  *
- * @author Paulo Henrique
+ * @author José Augusto Scherer
  */
-public class RentItWindow
-    extends
-        javax.swing.JFrame {
+public class 
+    RentItWindow 
+        extends javax.swing.JFrame {
 
-    /**
-     * Creates new form RentItWindow
-     */
     public RentItWindow() {
         initComponents();
+        
+        /*
+         * lp = LoginPane
+         */
+        LoginPane lp = new LoginPane();
+        lp.setSize(440, 440);
+        lp.setLocation(0, 0);
+        
+        content.removeAll();
+        content.add(lp, BorderLayout.CENTER);
+        content.revalidate();
+        content.repaint();
+ 
     }
 
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+
+        content = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        content.setMaximumSize(new java.awt.Dimension(440, 440));
+        content.setMinimumSize(new java.awt.Dimension(440, 440));
+
+        javax.swing.GroupLayout contentLayout = new javax.swing.GroupLayout(content);
+        content.setLayout(contentLayout);
+        contentLayout.setHorizontalGroup(
+            contentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 440, Short.MAX_VALUE)
+        );
+        contentLayout.setVerticalGroup(
+            contentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 440, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(content, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(content, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(0, 0, 0))
         );
 
         pack();
-    }// </editor-fold>
-    
-    // Variables declaration - do not modify
-    // End of variables declaration  
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+       
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new RentItWindow().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel content;
+    // End of variables declaration//GEN-END:variables
 }
