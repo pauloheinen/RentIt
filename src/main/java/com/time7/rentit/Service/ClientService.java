c45e1617b5c8a85d4c0e1fdd2badf7c262394810
Daniel Kistemacher da Silva <106277555+DanielKistemacher@users.noreply.github.com>
2023-05-09 21:25:40 -0300

    1.2 ClientService e ClientServiceInterface implementados

--- src/main/java/com/time7/rentit/Service/ClientService.java
+++ src/main/java/com/time7/rentit/Service/ClientService.java
@@ -1,17 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.time7.rentit.Service;
 
+import com.time7.rentit.Database.Connection;
+import com.time7.rentit.Database.Database;
 import com.time7.rentit.Entity.Client;
+import com.time7.rentit.Service.Interface.ClientServiceInterface;
 
 /**
  *
  * @author Daniel
  */
-public interface ClientService {
-    Client createClient (Client client) throws Exception;
-    void updateClient (Client client) throws Exception;
-    void deleteClient (Client client) throws Exception;    
+public class ClientService 
+    implements 
+        ClientServiceInterface {
+    
+    private static ClientService clientService;
+    
+    public static ClientService getInstance() {
+        if (clientService == null) {
+            clientService = new ClientService();
+        }
+        
+        return clientService;
+    }
+    
+    @Override
+    public Client createClient (Client client) throws Exception {
+        Database database = Database.getInstance();
+        
+        database.add(client);
+        
+        return null;
+    }
+    
+    @Override
+    public void updateClient (Client client) throws Exception {
+        Database database = Database.getInstance();
+        
+        Client find = database.findById(Client.class, client.getId());
+        
+        find.setName(client.getName());
+        find.setEmail(client.getEmail());
+        find.setPhone(client.getPhone());
+        find.setType(client.getType());
+        
+        database.update(find);
+    }
+    
+    @Override
+    public void deleteClient (Client client) throws Exception {
+        Database database = Database.getInstance();
+        
+        database.delete(client);
+    }
+    
+    public Client getClientById(int id) throws Exception {
+        Database database = Database.getInstance();
+     
+        return database.findById(Client.class, id);
+    }
+    
+    public Client getAll() throws Exception {
+        Connection connection = Connection.getConnection();
+        
+        String sql = "select e from clients";
+        
+        return connection.findObjectBySql(Client.class, sql);
+    }
 }
--- src/main/java/com/time7/rentit/Service/Interface/ClientServiceInterface.java
+++ src/main/java/com/time7/rentit/Service/Interface/ClientServiceInterface.java
@@ -1,35 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.time7.rentit.Service.Interface;
 
-import com.time7.rentit.Database.Database;
 import com.time7.rentit.Entity.Client;
-import com.time7.rentit.Service.ClientService;
-import jdk.jshell.spi.ExecutionControl;
 
 /**
  *
  * @author Daniel
  */
-public class ClientServiceInterface 
-    implements 
-        ClientService {
-    final private Database db = Database.getInstance();
-    
-    @Override
-    public Client createClient (Client client) throws Exception {
-        throw new ExecutionControl.NotImplementedException("not implemented");
-    }
-    
-    @Override
-    public void updateClient (Client client) throws Exception {
-        throw new ExecutionControl.NotImplementedException("not implemented");
-    }
-    
-    @Override
-    public void deleteClient (Client client) throws Exception {
-        throw new ExecutionControl.NotImplementedException("not implemented");
-    }
+public interface ClientServiceInterface {
+    Client createClient (Client client) throws Exception;
+    void updateClient (Client client) throws Exception;
+    void deleteClient (Client client) throws Exception;    
 }
